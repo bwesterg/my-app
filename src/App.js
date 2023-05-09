ca7ccf60cba39838a221f07ec8c58ff647fe53bd
bwesterg <benton.westergaard@gmail.com>
2023-05-09 17:28:06 -0600

    saving progress

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <Navbar />
         <Link>
           <Routes>
-            <Route path='/' exact ></Route>
+            <Route path='/' exact component={Home} /> 
           </Routes>
         </Link>
       </Router>
